de87e06ef3cf71a6f3be43bfeb5116a55b598e97
Nikunj <nikunj@example.com>
2025-05-29 12:40:05 +0530

    bug fixing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -62,7 +62,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     return res.status(201).json(
-        new ApiResponse(200,createdUser,"User Registered SucessFully");
+        new ApiResponse(200,createdUser,"User Registered SucessFully")
     )
 
 })
