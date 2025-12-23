8938628e742feb2c9ae18d9eaf14f91df5ecd688
Nikunj <nikunjgajera2550@gmail.com>
2025-12-23 13:21:06 +0530

    update .env.sample with environment variable placeholders; format ApiError class for consistency

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,23 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
         statusCode,
-        message ="Something Went Wrong",
-        errors  = [],
-        stack  =""
-    ){
+        message = "Something Went Wrong",
+        errors = [],
+        stack = ""
+    ) {
         super(message)
-        this.statusCode =statusCode
+        this.statusCode = statusCode
         this.data = null
         this.message = message
         this.success = false;
-        this.errors =errors;
+        this.errors = errors;
 
         if (stack) {
             this.stack = stack;
 
-        }else{
+        } else {
             Error
         }
     }
 }
-export {ApiError};
+export { ApiError };
